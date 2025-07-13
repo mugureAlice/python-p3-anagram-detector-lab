6d51b8c2f1e73891c6c95193fab0387ab6a6a4c6
AliceMugure <machariamugure17@gmail.com>
2025-07-13 09:08:14 +0300

    Anagram Detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self , original_word):
+        self.original_word= original_word
+
+    def match(self, current_word):
+        matches = []
+
+        for word in current_word:
+            if sorted(word)  == sorted(self.original_word):
+                matches.append(word)
+            
+        return matches
+
+
